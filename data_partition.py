0fc40063d23300be013f9934203d4c206fea918e
Haroon Rashid <haroon.rashid235@gmail.com>
2021-02-16 17:14:10 +0100

    Added partioning and code for scale experiment

---
+++ data_partition.py
@@ -0,0 +1,81 @@
+############### CONFIG ##############
+init_dataset_size = 50000
+snapshot_size = 10000
+split_on = ',' # Change the splitter character to what is suitable (e.g., '\t' )
+SWAPPED_COLUMNS = True # Switch to True, if the slowly changing column is to the right (should be the left one)
+######################################
+
+import networkx as nx
+from tqdm import tqdm
+file_path = 'NCI1/NCI1.edges'     #'data/Duke14.dat'#'data/amherst_nw.dat'
+new_init_file_path=''
+new_snap_file_path=''
+nodes =[]
+
+
+def gen_paths():
+    global new_init_file_path
+    global new_snap_file_path
+    path_without_suffix = file_path[:file_path.find('.')]
+    new_init_file_path = path_without_suffix+'.nw_init'
+    new_snap_file_path = path_without_suffix+'.nw_dynamic'
+
+def read_file(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    return lines[1:]
+
+def build_graph(edges:list):
+    # edges = read_file(file_path)
+    nodes_set=set()
+    g = nx.DiGraph()
+    for e in edges:
+        (a, b) = e.strip().split(split_on)
+        a,b = float(a), float(b)
+        nodes_set.add(int(a))
+        nodes_set.add(int(b))
+        if SWAPPED_COLUMNS:
+            g.add_edge(int(b), int(a))
+        else:
+            g.add_edge(int(a), int(b))
+
+    global nodes
+    nodes = list(nodes_set)
+    return g
+
+def partition_dataset(g:nx.Graph):
+    f1=open(new_init_file_path,"a+")
+    f1.truncate(0)
+    print(init_dataset_size, file=f1)
+    print('Generating initial dataset file..')
+    for n in tqdm(nodes[:init_dataset_size]):
+        for m in g.neighbors(n):
+            if m < init_dataset_size:
+                print(str(n)+' '+str(m) , file = f1)
+            else:
+                g.add_edge(m, n)
+    for n in nodes[:init_dataset_size]:
+        print(str(n) + ' ' + str(n), file=f1)
+    f1.close()
+    print('Done!')
+    f2 = open(new_snap_file_path, "a+")
+    f2.truncate(0)
+    print('Generating dynamic nodes file..')
+    for n in tqdm(nodes[init_dataset_size:]):
+        neighbors_list = []
+        for x in g.neighbors(n):
+            if x < n:
+                neighbors_list.append(x)
+            else:
+                g.add_edge(x, n)
+        neighbors_list.append(n)
+        print(str(n)+' '+str(len(neighbors_list)), file=f2)
+        for m in neighbors_list:
+            print(str(m)+' '+str(n), file = f2)
+        print('', file=f2)
+    f2.close()
+############ main ################
+gen_paths()
+e=read_file(file_path)
+partition_dataset(build_graph(e))
+print('Done!')
--- scale_experiment.py
+++ scale_experiment.py
@@ -2,33 +2,36 @@ import pandas as pd
 import numpy as np
 import random
 import math
-
 import networkx as nx
 from tqdm import tqdm
 import re
 import os
+import sys
 import matplotlib.pyplot as plt
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-
 from node2vec import Node2Vec
-
+import json
 import time
 import pickle
 
-random.seed(43)
+from collections import Counter
 
-
+########### Config ##########
+BINARY_CLASSIFICATION=False
 SHOW_PLOT = False
-OFFSET = 200
+#############################
+OFFSET_LIST = [10000]
 
-init_file = "data_large/Auburn71.nw_init"
-dynamic_file = 'data_large/Auburn71.nw_dynamic'
+
+init_file = "NCI1/NCI1.nw_init"
+dynamic_file = "NCI1/NCI1.nw_dynamic"
+
+ground_truth_file="NCI1/NCI1.node_labels"
 
 out_dir = 'output/'
 os.makedirs(out_dir, exist_ok=True)
@@ -40,243 +43,195 @@ def read_file(filename):
 
     return lines
 
+
+def load_json_from_file(filename):
+    return json.loads(open(filename).read())
+
+def load_ground_truth(file_path):
+    lst = []
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            items = line.split(',')
+            lst.append([int(float(i)) for i in items])
+    lst.sort()
+    l=[]
+    if BINARY_CLASSIFICATION:
+        l = [1 if i[1]>0 else 0 for i in lst]
+    else:
+        l = [i[1] for i in lst]
+
+    count_dict = Counter(l)
+    l = [i if count_dict[i] >= 1500 else 2 for i in l]
+    return l
+###########################
+
 def split_list(l, val=''):
     idx_list = [idx + 1 for idx, val in
-            enumerate(l) if val == '']
+                enumerate(l) if val == '']
 
-    splits = [l[i: j-1] for i, j in zip([0] + idx_list, idx_list + ([len(l)] if idx_list[-1] != len(l) else []))]
+    splits = [l[i: j - 1] for i, j in zip([0] + idx_list, idx_list + ([len(l)] if idx_list[-1] != len(l) else []))]
     return splits
 
-def clean_lists(lists, val='\t'):
+
+def clean_lists(lists):
     final_list = []
     for l in lists:
-        final_list.append([x.replace(val,',') for x in l][1:])
+        final_list.append([x.replace('\t', ',') for x in l][1:])
     return final_list
 
-def get_data(graph):
 
+def get_data(edge_list):
     # captture nodes in 2 separate lists
     node_list_1 = []
     node_list_2 = []
 
-    for i in tqdm(graph):
-      node_list_1.append(i.split(',')[0])
-      node_list_2.append(i.split(',')[1])
+    for i in tqdm(edge_list):
+        node_list_1.append(i.split(' ')[0])
+        node_list_2.append(i.split(' ')[1])
 
     graph_df = pd.DataFrame({'node_1': node_list_1, 'node_2': node_list_2})
-
-    print(graph_df.head())
+    # print(graph_df.head())
 
     # create graph
     G = nx.from_pandas_edgelist(graph_df, "node_1", "node_2", create_using=nx.Graph())
-
-    # plot graph
-    plt.figure(figsize=(10,10))
-
-    pos = nx.random_layout(G, seed=23)
-    nx.draw(G, with_labels=True,  pos = pos, node_size = 40, alpha = 0.6, width = 0.7)
-
-    if SHOW_PLOT:
-        plt.show()
 
     # combine all nodes in a list
     node_list = node_list_1 + node_list_2
 
     # remove duplicate items from the list
     node_list = list(dict.fromkeys(node_list))
+    all_labels = load_ground_truth(ground_truth_file)
+    labels = [all_labels[int(i)] for i in node_list]
+    data = pd.DataFrame({'node':node_list, 'label':labels})
+    return G, data
 
-    # build adjacency matrix
-    adj_G = nx.to_numpy_matrix(G, nodelist = node_list)
-    print(f"Adj Matrix Shape: {adj_G.shape}")
-
-
-    # get unconnected node-pairs
-    all_unconnected_pairs = []
-
-    # Traverse adjacency matrix
-    offset = 0
-    for i in tqdm(range(adj_G.shape[0])):
-        for j in range(offset,adj_G.shape[1]):
-            if i != j:
-                if nx.has_path(G, node_list[i],node_list[j]):
-                    if nx.shortest_path_length(G, node_list[i], node_list[j]) <=2:
-                        if adj_G[i,j] == 0:
-                            all_unconnected_pairs.append([node_list[i],node_list[j]])
-                # else:
-                #   print(f"No path between {i} and {j}")
-
-        offset = offset + 1
-
-    print(f"All Unconnected Pairs: {len(all_unconnected_pairs)}")
-
-    node_1_unlinked = [i[0] for i in all_unconnected_pairs]
-    node_2_unlinked = [i[1] for i in all_unconnected_pairs]
-
-    data = pd.DataFrame({'node_1':node_1_unlinked, 
-                         'node_2':node_2_unlinked})
-
-    # add target variable 'link'
-    data['link'] = 0
-
-    print("Data Dataframe:")
-    print(data.head())
-
-
-    initial_node_count = len(G.nodes)
-    initial_connected_comps = nx.number_connected_components(G)
-
-    graph_df_temp = graph_df.copy()
-
-    # empty list to store removable links
-    omissible_links_index = []
-
-    for i in tqdm(graph_df.index.values):
-      
-        # remove a node pair and build a new graph
-        G_temp = nx.from_pandas_edgelist(graph_df_temp.drop(index = i), "node_1", "node_2", create_using=nx.Graph())
-
-        # check there is no spliting of graph and number of nodes is same
-        if (nx.number_connected_components(G_temp) == initial_connected_comps) and (len(G_temp.nodes) == initial_node_count):
-            # print("HERE")
-            omissible_links_index.append(i)
-            graph_df_temp = graph_df_temp.drop(index = i)
-
-    print(f"Omissible Links: {len(omissible_links_index)}")
-
-
-    # create dataframe of removable edges
-    graph_df_ghost = graph_df.loc[omissible_links_index]
-
-    # add the target variable 'link'
-    graph_df_ghost['link'] = 1
-
-    data = data.append(graph_df_ghost[['node_1', 'node_2', 'link']], ignore_index=True)
-
-    print(data['link'].value_counts())
-
-    ## FEATURE EXTRACTION
-    # drop removable edges
-    graph_df_partial = graph_df.drop(index=graph_df_ghost.index.values)
-
-    # build graph
-    G_data = nx.from_pandas_edgelist(graph_df_partial, "node_1", "node_2", create_using=nx.Graph())
-
-    return G_data, data
 
 def get_embeddings(G_data, data):
-
     # Generate walks
     node2vec = Node2Vec(G_data, dimensions=20, walk_length=16, num_walks=50)
 
     # train node2vec model
     n2w_model = node2vec.fit(window=7, min_count=1)
-
-    x = [(n2w_model[str(i)]+n2w_model[str(j)]) for i,j in zip(data['node_1'], data['node_2'])]
+    x = [n2w_model[str(i)] for i in data['node']]
 
     return x, data
 
 
 def test_embeddings(x, data):
-    xtrain, xtest, ytrain, ytest = train_test_split(np.array(x), data['link'], 
-                                                    test_size = 0.3, 
-                                                    random_state = 35)
+    scaler = preprocessing.StandardScaler().fit(x)
+    x_scaled = scaler.transform(x)
+    xtrain, xtest, ytrain, ytest = train_test_split(np.array(x_scaled), data['label'],
+                                                    test_size=0.3,
+                                                    random_state=35,stratify=data['label'])
 
     print("Running Logistics Regression on the embeddings...")
     lr = LogisticRegression(class_weight="balanced", max_iter=1000)
     lr.fit(xtrain, ytrain)
     predictions = lr.predict_proba(xtest)
-
     # print("Running random forest on the embeddings...")
     # model = RandomForestClassifier(max_depth=5, random_state=0, n_estimators=50)
     # model.fit(xtrain, ytrain)
     # preds = model.predict_proba(xtest)
 
-    auc_score = roc_auc_score(ytest, predictions[:,1])
+    auc_score = roc_auc_score(ytest, predictions, multi_class='ovr')
     print(f"AUC ROC score for LogisticRegression: {auc_score}")
     return auc_score
     # print(f"AUC ROC score for RandomForest: {roc_auc_score(ytest, preds[:,1])}")
 
-g_init = read_file(init_file)
-g_dyn = read_file(dynamic_file)
+def run_experiment(OFFSET):
 
-dynamic_splits = split_list(g_dyn, val='')
-NUM_SNAPSHOTS = math.ceil(len(dynamic_splits)/OFFSET)
+    g_init = read_file(init_file)
+    g_dyn = read_file(dynamic_file)
 
-print(f"OFFSET: {OFFSET}")
-print(f"NUM_SNAPSHOTS: {NUM_SNAPSHOTS}")
+    dynamic_splits = split_list(g_dyn, val='')
 
-num_nodes = g_init[0]
-g_init = g_init[1:]
+    num_nodes = g_init[0]
+    g_init = g_init[1:1000]
 
-g_init = clean_lists([g_init],val=" ")[0]
-dynamic_splits = clean_lists(dynamic_splits, val=" ")
+    g_init = clean_lists([g_init])[0]
+    dynamic_splits = clean_lists(dynamic_splits)
 
+    NUM_SNAPSHOTS = math.ceil(len(dynamic_splits) / OFFSET)
+    
+    print(f"NUM_SNAPSHOTS: {NUM_SNAPSHOTS}, OFFSET: {OFFSET}")
 
-print(f"No. of Nodes: {num_nodes}, No. of links: {len(g_init)}")
+    print(f"No. of Nodes: {num_nodes}, No. of links: {len(g_init)}")
+    auc_scores = []
+    times = {}
 
-auc_scores = []
-times = {}
+    G_data, data = get_data(g_init)
 
-
-G_data, data = get_data(g_init)
-start_time = time.time()
-x, data = get_embeddings(G_data, data)
-end_time = time.time()
-
-
-auc_score = test_embeddings(x, data)
-
-
-print(f"Initial Graph with {num_nodes} nodes took {end_time - start_time} ms for embeddings...")
-
-times[num_nodes] = end_time - start_time
-
-
-START = 0
-END = START + OFFSET
-
-
-
-
-new_graph = g_init
-current_snapshot = 1
-
-auc_scores.append(auc_score)
-
-while END < NUM_SNAPSHOTS * OFFSET:#len(dynamic_splits):
-    new_splits = dynamic_splits[START:END]
-    new_edges = [item for sublist in new_splits for item in sublist]
-
-    new_graph += new_edges
-
-    print(f"Snapshot: {current_snapshot}")
-    print(f"No. of Nodes: {int(num_nodes)+END}, No. of links: {len(new_graph)}")
-
-    current_snapshot += 1
-
-    G_data, data = get_data(new_graph)
     start_time = time.time()
-    x, data = get_embeddings(G_data, data)
-    end_time = time.time()
 
-    print(f"Dynamic Graph with {int(num_nodes)+END} nodes took {end_time - start_time} ms for embeddings...")
-    times[str(int(num_nodes)+END)] = end_time - start_time
+    x, data = get_embeddings(G_data, data)
+
+    end_time = time.time()
+    auc_score = test_embeddings(x, data)
+
+    print(f"Initial Graph with {num_nodes} nodes took {end_time - start_time} ms for embeddings...")
+
+    times[num_nodes] = end_time - start_time
+
+
+    START = 0
+    END = START + OFFSET
 
     
-    auc_score = test_embeddings(x, data)
+    new_graph = g_init
+    current_snapshot = 1
 
     auc_scores.append(auc_score)
 
-    START = END
-    END = START + OFFSET
+    while END < NUM_SNAPSHOTS * OFFSET:  # len(dynamic_splits):
+        new_splits = dynamic_splits[START:END]
+        new_edges = [item for sublist in new_splits for item in sublist]
+
+        new_graph += new_edges
+
+        print(f"Snapshot: {current_snapshot}")
+        print(f"No. of Nodes: {int(num_nodes) + END}, No. of links: {len(new_graph)}")
+
+        current_snapshot += 1
+
+        G_data, data = get_data(new_graph)
+        start_time = time.time()
+        x, data = get_embeddings(G_data, data)
+
+        end_time = time.time()
+
+        print(f"Dynamic Graph with {int(num_nodes) + END} nodes took {end_time - start_time} ms for embeddings...")
+        times[int(num_nodes) + END] = end_time - start_time
+
+        auc_score = test_embeddings(x, data)
+        auc_scores.append(auc_score)
+
+        START = END
+        END = START + OFFSET
+
+    print(f"AUC scores: {auc_scores}")
+    print(f"Times Dict: {times}")
+
+    return auc_scores, times
 
 
-print(f"AUC scores: {auc_scores}")
-print(f"Times Dict: {times}")
+auc_scores_list = []
+times = []
+for offset in OFFSET_LIST:
+    auc_score, times_dict = run_experiment(offset)
+    auc_scores_list.append(auc_score)
+    times.append(times_dict)
+
 
 with open(os.path.join(out_dir, 'auc_scores.pkl'), 'wb') as handle:
-    pickle.dump(auc_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+        pickle.dump(auc_scores_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 with open(os.path.join(out_dir, 'times_dict.pkl'), 'wb') as handle:
     pickle.dump(times, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+print("Experiment Finished...")
+print(f"AUC scores List: {auc_scores_list}")
+print(f"Times Dict: {times}")
